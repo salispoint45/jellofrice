8293031aae76e5c5fa5d17a9a2d64b26e392c2a2
salispoint45 <danama45@LAPTOP-0R63HNPH.localdomain>
2022-01-17 22:09:29 -0500

    assignment_jan17.py

---
+++ assignment_jan17.py
@@ -0,0 +1,21 @@
+# 1. Which of the following is an operator and which is a value
+# * is an operator
+# 'Hello' is a value
+# -88.88 is a value
+# - is an operator
+# / is an operator
+# + is an operator
+# 5 is a value
+# - is an operator
+# 2. Which of these is a variable and which is a string
+# spam is a variable
+# 'spam' is a string
+# 3. Name three data types
+   # Integal
+   # Floating point numbers
+   # Character
+# 4. what is an expression made of. What do all expressions do?
+# An expression is a construct made up of variables, operators, and method invocations, which are constructed according to the syntax of the syntax of the language, that evaluates to a single value. An expression need to be evaluted. onece you ask python to print an expression, the interpreter evalutes the expression and dispay the result.
+# 5. What does the variable bacon contain after running the the code below
+bacon = 20 #The bacon is set to 20
+bacon + 1 #  Expresion does not reassign the value in bacon it means that would need an assignment statement: bacon = bacon +1
