a311afc5b7f3c39af153cb3301869e5ff288043c
salispoint45 <obisco45@yahoo.com>
2022-01-20 22:16:16 -0500

    Boolean

---
+++ assignment_jan19.py
@@ -0,0 +1,20 @@
+# #What are the two values of a boolean data type. How do you write them.
+
+# Python boolean type is one of the built-in data types provided by Python, which represents one of the two values i.e. True or False. Generally, it is used to represent the truth values of the expressions. For example, 1== 0 is True whereas 2<1 is False.
+
+#What are the three boolean operators?
+
+#AND
+#OR
+#NOT
+
+#Write out the truth tables of both boolean operators i.e every possible combination of boolean values and their result.
+
+
+#What are the six comparison operators.
+
+# less than ( < ), less than or equal to ( <= ), greater than ( > ), greater than or equal to ( >= ), equal to ( == ), and not equal to ( != ).
+
+#What is the difference between the equal operator and the assignment operator
+
+# The difference between = (Assignment) and == (Equal to) operators. The “=” is an assignment operator is used to assign the value on the right to the variable on the left. The '==' operator checks whether the two given operands are equal or not. If so, it returns true.
